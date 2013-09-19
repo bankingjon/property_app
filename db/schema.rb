6d884b4cec651f15f733cc7c5dd409ea769dfc3c
Jonathan Meyer <jlmeyer@gmail.com>
2013-09-19 14:33:26 -0500

    Remove Location Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919190956) do
-
-  create_table "locations", force: true do |t|
-    t.string   "name"
-    t.integer  "address"
-    t.string   "type"
-    t.integer  "owner"
-    t.integer  "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
